abe161ef7010bf351bfe9e4110da65e8632cab4e
Kolpic <galincho112@gmail.com>
2022-12-09 08:22:18 +0200

    Added new task

---
+++ LabInterfacesAndAbstraction/Ferrari/Ferrari.java
@@ -0,0 +1,25 @@
+package Ferrari;
+
+public class Ferrari implements Car{
+
+    private String driverName;
+
+    private String model = "488-Spider";
+
+    public Ferrari(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String brakes() {
+        return "Brakes!";
+    }
+
+    public String gas() {
+        return "brum-brum-brum-brrrrr";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s/%s/%s",model,brakes(),gas(),driverName);
+    }
+}
