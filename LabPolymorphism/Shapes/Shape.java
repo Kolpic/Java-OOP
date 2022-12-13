cb31db08372d6d3a710ebd742597d3005518b2c4
Kolpic <galincho112@gmail.com>
2022-12-13 22:50:06 +0200

    ADD

---
+++ LabPolymorphism/Shapes/Shape.java
@@ -0,0 +1,27 @@
+package Shapes;
+
+public abstract class Shape {
+
+    private Double perimeter;
+    private Double area;
+
+    protected void setPerimeter(Double perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    protected void setArea(Double area) {
+        this.area = area;
+    }
+
+    public Double getArea() {
+        return this.area;
+    }
+
+    public Double getPerimeter() {
+        return this.perimeter;
+    }
+
+    protected abstract void calculatePerimeter();
+
+    protected abstract void calculateArea();
+}
