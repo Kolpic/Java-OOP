ea6847ef19edcbd30483306e3c6ffdc59688302a
Kolpic <galincho112@gmail.com>
2022-12-23 17:32:54 +0200

    Added new task

---
+++ test-driven-development-exercise/src/main/java/Transaction.java
@@ -0,0 +1,14 @@
+public interface Transaction {
+
+    int getId();
+
+    void setStatus(TransactionStatus newStatus);
+
+    TransactionStatus getStatus();
+
+    double getAmount();
+
+    String sentFrom();
+
+    String sentTo();
+}
