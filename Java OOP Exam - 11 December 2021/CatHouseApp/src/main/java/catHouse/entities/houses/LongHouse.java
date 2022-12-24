884d0f43bcd2a07fbf4284ee6e06067301cb5a5e
Kolpic <galincho112@gmail.com>
2022-12-24 14:11:43 +0200

    Added new task

---
+++ Java OOP Exam - 11 December 2021/CatHouseApp/src/main/java/catHouse/entities/houses/LongHouse.java
@@ -0,0 +1,10 @@
+package catHouse.entities.houses;
+
+public class LongHouse extends BaseHouse{
+
+    private final static int CAPACITY = 30;
+
+    public LongHouse(String name) {
+        super(name, CAPACITY);
+    }
+}
